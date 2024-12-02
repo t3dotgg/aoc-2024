a011b71b0f59d5fb18702c4c63ab4aa772fe06b2
Theo Browne <me@t3.gg>
2024-12-01 21:13:10 -0800

    cleaned up

--- 2/index.ts
+++ 2/index.ts
@@ -2,37 +2,6 @@ import fs from "fs";
 
 const lines = fs.readFileSync("input.txt", "utf8").split("\n");
 
-function a() {
-  console.log(lines.length);
-
-  const nums = lines.map((r) => r.split(" ").map((n) => parseInt(n, 10)));
-
-  const safe = nums.filter((numlist) => {
-    const allIncrease = numlist.every((n, i) => i === 0 || n > numlist[i - 1]);
-    const allDecrease = numlist.every((n, i) => i === 0 || n < numlist[i - 1]);
-    return allIncrease || allDecrease;
-  });
-
-  safe.forEach((numlist) => {
-    console.log(numlist.join(" "));
-  });
-
-  const safe2 = safe.filter((numlist) => {
-    // make sure every increase/decrease is 1-3 numbers apart
-    const diffs = numlist.map((n, i) => n - numlist[i - 1]);
-    diffs.shift();
-    console.log(
-      "comparing",
-      numlist,
-      diffs,
-      diffs.every((d) => d >= -3 && d <= 3)
-    );
-    return diffs.every((d) => d >= -3 && d <= 3);
-  });
-
-  console.log(safe2.length);
-}
-
 // Check that all numbers increase or decrease by 1-3
 function safeLine(numlist: number[]) {
   const diffs = numlist.map((n, i) => n - numlist[i - 1]);
@@ -40,6 +9,14 @@ function safeLine(numlist: number[]) {
   return (
     diffs.every((d) => d >= -3 && d < 0) || diffs.every((d) => d <= 3 && d > 0)
   );
+}
+
+function a() {
+  const nums = lines.map((r) => r.split(" ").map((n) => parseInt(n, 10)));
+
+  const safe = nums.filter((numlist) => safeLine(numlist));
+
+  console.log(safe.length);
 }
 
 function b() {
